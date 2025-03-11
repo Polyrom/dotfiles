7851b638956148cacd5a9d8e2eebfb3622a7dcc9
Roman Poliakov <roman61394@gmail.com>
2025-03-12 00:15:50 +0300

    go full kanagawabones

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -782,18 +782,20 @@ require('lazy').setup({
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
   {
-    'rose-pine/neovim',
-    name = 'rose-pine',
+    'zenbones-theme/zenbones.nvim',
+    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    dependencies = 'rktjmp/lush.nvim',
     lazy = false,
     priority = 1000,
+    -- you can set set configuration options here
     config = function()
-      require('rose-pine').setup {
-        styles = { italic = false, bold = false, dim_inactive_windows = true },
-        highlight_groups = { Comment = { italic = true } },
-      }
-      vim.cmd.colorscheme 'rose-pine'
+      vim.g.zenbones_darken_comments = 45
+      vim.cmd.colorscheme 'kanagawabones'
     end,
   },
+  -- example lazy.nvim install setup
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
